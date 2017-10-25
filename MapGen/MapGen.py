4107c807dc718d99c6435b307fa7600561fa68db
BerenWallis <32566086+BerenWallis@users.noreply.github.com>
2017-10-25 16:02:06 +0100

    Finished project
    Code put together pep-8 followed. Project finished

--- MapGen/MapGen.py
+++ MapGen/MapGen.py
@@ -1,11 +1,9 @@
 import pygame
 import random
-import os
-print os.getcwd()
+
 
 class MAP:
-    FOREST_RADIUS_MAX = 10
-    FOREST_AMOUNT = 3
+    SEA_CHANCE = 20
     BLOCK_SIZE = 80
     SIZE_Y = 60
     SIZE_X = 60
@@ -16,19 +14,25 @@ class MAP:
         [2, "Images/Ground/temp_rock.jpg"],
         [1, "Images/Ground/temp_spooky.jpg"]
     ]
+    SEA_TILE = [  # Information on sea tiles (filename)
+                ["Images/Sea/Sand.jpg"],
+                ["Images/Sea/Sea.jpg"],
+        ]
+
 
 class MapClass:
     """Fully extendible mapclass, image size and spawn weights can be edited"""
-    map = [[0 for x in range(0, MAP.SIZE_X)] for y in range(0, MAP.SIZE_X)]  # generates a 2d array for Map size
-    rivers = [[False for x in range(0, MAP.SIZE_X)] for y in range(0, MAP.SIZE_X)]
+    map = [[0 for x in range(0, MAP.SIZE_X)] for y in range(0, MAP.SIZE_Y)]  # Generates a 2d array for Map size
+    sea = [[False for x in range(0, MAP.SIZE_X+2)] for y in range(0, MAP.SIZE_Y+2)]
     img = pygame.Surface((MAP.SIZE_X, MAP.SIZE_Y))
 
-    def __init__(self, seed=0):  # Initilizes Map class with a seed
+    def __init__(self, seed=0):
+        """Initilizes Map class with a seed"""
         if not (seed == 0):
             random.seed(seed)
         total_weight = 0
         for i in MAP.TILE_INFO:
-            total_weight += i[0]
+            total_weight += i[0]  # Gets the total weight if everything in MAP.TILE_INFO
         for y in range(0, MAP.SIZE_Y):
             for x in range(0, MAP.SIZE_X):
                 rand = random.randint(0, total_weight)
@@ -39,8 +43,11 @@ class MapClass:
                         ndone = False
                         self.map[x][y] = i
         self.map_render()
+        self.create_sea()
+        self.img = self.sea_render()
 
     def map_render(self):
+        """Renders map array into img surface"""
         ret = pygame.Surface((MAP.SIZE_X * MAP.BLOCK_SIZE, MAP.SIZE_Y * MAP.BLOCK_SIZE))
         for y in range(0, MAP.SIZE_Y):
             for x in range(0, MAP.SIZE_X):
@@ -48,14 +55,90 @@ class MapClass:
                 ret.blit(temp_img, (x * MAP.BLOCK_SIZE, y * MAP.BLOCK_SIZE))
         self.img = ret
 
+    def create_sea(self):
+        """Checks each on x,1 and 1,y to see if a sea starts, 1 is used as the array has a boarder"""
+        for y in range(1, MAP.SIZE_Y + 1):  # Spawns sea starts on the y axis
+            number = random.randint(0, MAP.SEA_CHANCE)  # Generate random number to see if sea spawns
+            if number == 0:  # If number is 0 change array position to True and run sea_flow_y
+                self.sea[1][y] = True
+                MapClass.sea_flow_y(self, y)
+        for x in range(1, MAP.SIZE_X + 1):  # Spawns sea starts on the x axis
+            number = random.randint(0, MAP.SEA_CHANCE)
+            if number == 0:  # If number is 0 change array position to True and run sea_flow_x
+                self.sea[x][1] = True
+                MapClass.sea_flow_x(self, x)
+
+    def sea_flow_y(self, y):
+        """Loops placing True in the array until hitting the edge of array,
+        randomly picks the direction starting on y axis"""
+        x = 1
+        while not x >= MAP.SIZE_X and not x < 1 and not y >= MAP.SIZE_Y and not y < 1:
+            # While not past the array boundary's
+            which_tile = random.randint(0, 3)  # Random int used to pick which direction
+            if which_tile == 0:
+                y -= 1
+                self.sea[x][y] = True  # Up
+            if which_tile == 1:
+                y += 1
+                self.sea[x][y] = True  # Down
+            if which_tile >= 2:
+                x += 1
+                self.sea[x][y] = True  # Right
+
+    def sea_flow_x(self, x):
+        """Loops placing True in the array until hitting the edge of array,
+        randomly picks the direction starting on x axis"""
+        y = 1
+        while not y >= MAP.SIZE_Y and not y < 1 and not x >= MAP.SIZE_X and not x < 1:
+            # While not past the array boundary's
+            which_tile = random.randint(0, 3)  # Random int used to pick which direction
+            if which_tile == 0:
+                x -= 1
+                self.sea[x][y] = True  # Left
+            if which_tile == 1:
+                x += 1
+                self.sea[x][y] = True  # Right
+            if which_tile >= 2:
+                y += 1
+                self.sea[x][y] = True  # Down
+
+    def sea_render(self):
+        """Blits sea images depending on what other places adjacent are seas and sand to some of the edges"""
+        surf = self.img
+        for y in range(0, MAP.SIZE_Y):
+            for x in range(0, MAP.SIZE_X):
+                adj = MapClass.sea_check(self, x, y)  # Runs function to check whats next to current tile
+                if adj == 1 or adj == 10 or adj == 100 or adj == 1000:  # If at the edge of the sea
+                    temp_riv = pygame.image.load(MAP.SEA_TILE[0][0]).convert()  # Get sand
+                    surf.blit(temp_riv, ((x * MAP.BLOCK_SIZE), (y * MAP.BLOCK_SIZE)))
+                elif adj != 0 and adj != 1 and adj != 10 and adj != 100 and adj != 1000:  # If multiple sea connections
+                    temp_riv = pygame.image.load(MAP.SEA_TILE[1][0]).convert()
+                    surf.blit(temp_riv, ((x * MAP.BLOCK_SIZE), (y * MAP.BLOCK_SIZE)))
+
+        return surf  # Return edited image
+
+    def sea_check(self, x, y):
+        """Checks adjacent tiles for seas"""
+        num_adj = 0  # Variable to return
+        if self.sea[x - 1][y]:  # Check left
+            num_adj += 1
+        if self.sea[x][y + 1]:  # Check up
+            num_adj += 10
+        if self.sea[x][y - 1]:  # Check down
+            num_adj += 100
+        if self.sea[x + 1][y]:  # Check right
+            num_adj += 1000
+        return num_adj
+
 
 screen = pygame.display.set_mode((800, 500))
 Map = MapClass()
 pygame.init()
-temp_scroll = [0, 0]  # TEMP SCROLL MECHANIC
+temp_scroll = [0, 0]  # DELETE WHEN BUILT (Temporary scroll mechanic)
 
 
 def display():
+    """displays generated map"""
     done = False
     while not done:
         for event in pygame.event.get():
@@ -64,7 +147,7 @@ def display():
             if event.type == pygame.KEYDOWN and event.key == pygame.K_ESCAPE:
                 done = True
 
-            """TEMPORARY SCROLL MECHANIC"""
+            """DELETE WHEN BUILT (Temporary scroll mechanic)"""
             if event.type == pygame.KEYDOWN:
                 if event.key == pygame.K_LEFT:
                     if temp_scroll[0] > 0:
@@ -81,7 +164,7 @@ def display():
                 if event.key == pygame.K_DOWN:
                     if temp_scroll[1] < (MAP.SIZE_Y * MAP.BLOCK_SIZE - 500):
                         temp_scroll[1] += 50
-            """END TEMP SCROLL MECHANIC"""
+            """END -- DELETE WHEN BUILT (Temporary scroll mechanic)"""
 
         screen.blit(Map.img, (-temp_scroll[0], -temp_scroll[1]))
         pygame.display.flip()
